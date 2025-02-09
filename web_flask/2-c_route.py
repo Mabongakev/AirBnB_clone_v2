b732ed68240cfc0b8422fd767b5f286fcd0cc680
Mabongakev <kelvin.mabonga92@gmail.com>
2025-02-09 12:18:21 -0800

    Adding text to the route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,32 @@
-#!usr/bin/python3
-"""Script that starts a Flask web application"""
+#!/usr/bin/python3
+
+"""
+A script to Start a Flask web application
+"""
 
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-app.route("/")
-def hello():
-    """Function to print Hello HBNB!"""
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Root path"""
     return "Hello HBNB!"
 
-app.route("/hbnb")
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Function to print HBNB"""
+    """hbnb path"""
     return "HBNB"
 
-app.route("/c/<text>")
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """c text path"""
+    return f"C {escape(text).replace('_', ' ')}"
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
