27f98742a530318f2c3dfffb333642e1d6632f94
Mabongakev <kelvin.mabonga92@gmail.com>
2025-02-09 12:07:31 -0800

    using the HBNB on the route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,25 @@
-#!usr/bin/python3
-"""Script that starts a Flask web application"""
+#!/usr/bin/python3
+
+"""
+A script to Starts Flask web application
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello():
-    """Function to print Hello HBNB!"""
+def hello_hbnb():
+    """Display the Root path"""
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Function to print HBNB"""
+    """display the hbnb path"""
     return "HBNB"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
